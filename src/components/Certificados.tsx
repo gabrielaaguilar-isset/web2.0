433c992611cfeeb185e4d50809121962aeed34d9
Gabriela Aguilar <contacto.gabriela.aguilar@gmail.com>
2024-09-17 20:16:03 -0500

    version de 2000 arreglada

--- src/components/Certificados.tsx
+++ src/components/Certificados.tsx
@@ -5,7 +5,7 @@ import Titles from "./Titles";
 
 const Certificados = () => {
   return (
-    <div className="mb-[100px] mx-[20px] lg:px-[100px] 2xl:px-[150px] pb-10 ">
+    <div className="mb-[100px] mx-[20px] lg:px-[100px] 2xl:px-[150px] pb-10 max-w-[1920px] 2xl:m-auto 2xl:mb-[100px]">
       <div className=" flex flex-col lg:flex-row    lg:gap-44 lg:justify-between ">
         <div className="max-w-[207px] lg:max-w-[277px]">
           <Titles text="Certificados" customClass="text-left" />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,31 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="bg-black py-12 px-[50px] lg:px-[100px] 2xl:px-[150px] rounded-t-[55px] flex flex-col sm:flex-row   items-center sm:justify-between">
+    <div className="bg-black py-12 px-[50px] lg:px-[100px] 2xl:px-[150px] rounded-t-[55px]  ">
+      <div className="flex  flex-col sm:flex-row   items-center sm:justify-between max-w-[1920px] m-auto">
         <div className="flex justify-center gap-8">
-            <Link className="text-[16px] font-regular text-white/70 " href="https://www.linkedin.com/in/gabriela-aguilar01/" target="_blank" >Linkedin</Link>
-            <Link className="text-[16px] font-regular text-white/70 " href="mailto:contacto.gabriela.aguilar@gmail.com" target="_blank" >Email</Link>
-
-
+          <Link
+            className="text-[16px] font-regular text-white/70 "
+            href="https://www.linkedin.com/in/gabriela-aguilar01/"
+            target="_blank"
+          >
+            Linkedin
+          </Link>
+          <Link
+            className="text-[16px] font-regular text-white/70 "
+            href="mailto:contacto.gabriela.aguilar@gmail.com"
+            target="_blank"
+          >
+            Email
+          </Link>
         </div>
-        <h3 className="text-[18px] font-regular text-white/80 mt-2">Gabriela Aguilar © 2024</h3>
+        <h3 className="text-[18px] font-regular text-white/80 mt-2">
+          Gabriela Aguilar © 2024
+        </h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
