bba1ca4d0fbfd4cc7c5abf22f82cc1ca003681aa
Gabriela Aguilar <contacto.gabriela.aguilar@gmail.com>
2024-10-03 17:42:45 -0500

    cambios

--- src/components/FinalSection.tsx
+++ src/components/FinalSection.tsx
@@ -1,21 +1,30 @@
+import Link from "next/link";
 import imgFinalDesk from "../assets/images/formaDesk.svg";
 import imgFinalmobile from "../assets/images/imgFinal.svg";
 import Image from "next/image";
 const FinalSection = () => {
   return (
-<<<<<<< HEAD
-    <div className="BackgroundNoise flex flex-col sm:flex-row px-[20px] lg:px-[100px] 2xl:px-[150px] mb-10 items-center py-16 ">
-=======
-    <div className="BackgroundNoise ">
-      <div className="max-w-[1920px] m-auto flex flex-col sm:flex-row px-[20px] lg:px-[100px] 2xl:px-[150px] mb-10 items-center py-16 ">
->>>>>>> 433c992611cfeeb185e4d50809121962aeed34d9
+    <div className="BackgroundNoise">
+      <div className="  flex flex-col sm:flex-row px-[20px] lg:px-[100px] 2xl:px-[150px] mb-10 items-center py-16 ">
       <Image src={imgFinalmobile} alt="forma decorativa" className=" -mt-10 lg:hidden" />
       <Image src={imgFinalDesk} alt="forma decorativa" className=" -mt-10 hidden lg:block lg:mr-10" />
 
+      <div className="">
       <h5 className="text-[20px] lg:text-[24px]">Si estás buscando soluciones web innovadoras y personalizadas, estoy aquí para hacer realidad tus ideas. No dudes en escribirme un correo o visitar mi perfil de LinkedIn. ¡Hagamos que tu proyecto sea un éxito! 😊😊</h5>
       </div>
-
-    </div>
+     
+      </div>
+      <div className="mt-8">
+            <Link
+              href="https://api.whatsapp.com/send/?phone=573133207188&text&type=phone_number&app_absent=0"
+              target="_blank"
+              className="bg-black text-white py-[16px] px-[42px] rounded-[35px] mt-14 hover:bg-purple transition-all duration-300"
+            >
+              WhatsApp
+            </Link>
+          </div>
+      </div>
+      
   );
 };
 
